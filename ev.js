cc06d74f2834d3e6bcc8b7e48b9a53d1ff6fce7d
arm727 <armenmiqayelyan777@gmail.com>
2017-05-18 20:54:59 +0400

    ev changes

--- ev.js
+++ ev.js
@@ -1,9 +1,15 @@
 'use strict';
-let counter=0;
+// let counter=0;
+//
+// const interval = setInterval(function(){
+//   console.log(`Called ${++counter}times`);
+//   if(counter == 4){
+//     clearInterval(interval)
+//   }
+// },1000)
 
-const interval = setInterval(function(){
-  console.log(`Called ${++counter}times`);
-  if(counter == 4){
-    clearInterval(interval)
-  }
-},1000)
+process.stdin.on('data',function(d){
+  console.log(d);
+  console.log(d.toString().toUpperCase())
+
+})

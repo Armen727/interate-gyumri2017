be240004d9604473f9f14c8c20fa8b375f410c29
arm727 <armenmiqayelyan777@gmail.com>
2017-05-18 19:16:49 +0400

    index change

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 'use strict';
 console.log('Hellow world!');
+
+
+const speak = function(){
+  const arr = ['fool','bar','baz'];
+  for(const i of arr){
+    console.log(i.toUpperCase());
+  }
+}
+speak();

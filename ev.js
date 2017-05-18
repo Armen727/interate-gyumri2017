ded5ae4a0e8314b8b8c5e6b20985757da2be26a2
arm727 <armenmiqayelyan777@gmail.com>
2017-05-18 20:30:24 +0400

    stop interval

--- ev.js
+++ ev.js
@@ -1,5 +1,9 @@
 'use strict';
+let counter=0;
 
-setInterval(function(){
-  console.log('Hello with delay :)')
-},3000)
+const interval = setInterval(function(){
+  console.log(`Called ${++counter}times`);
+  if(counter == 4){
+    clearInterval(interval)
+  }
+},1000)

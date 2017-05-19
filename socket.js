5e7308b046dea0b222ae972b9bd010709d098928
arm727 <armenmiqayelyan777@gmail.com>
2017-05-19 20:18:55 +0400

    socket

---
+++ socket.js
@@ -0,0 +1,18 @@
+const net = require('net');
+const sock = new net.Socket;
+
+//we connected, new start reading data.
+sock.on('connect',() => {
+  sock.write('GET /en/ HTTP/1.1 \r\n');
+  sock.write('Host: istc.am');
+  sock.write('\r\n\r\n');
+});
+sock.setEncoding('utf-8');
+const total_data=[];
+sock.on('data', d => total_data.push(d));
+sock.on('end',
+  () => console.log(total_data.reduce((prev, total) => prev + total)));
+  sock.connect({
+  port:80,
+  host:'istc.am'
+})
